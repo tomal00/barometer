9414c2e64e645e220c673e28fbbcfeb8e2fab9f3
Kendaboi <kenny282000@seznam.cz>
2018-02-04 23:23:48 +0100

    d
    d

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
+/*eslint brace-style: ["error", "1tbs", { "allowSingleLine": true }]*/
+/*eslint brace-style: ["error", "stroustrup"]*/
+
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
@@ -5,92 +8,175 @@ const mysql = require('mysql');
 
 const app = express();
 const server = http.createServer(app);
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '1a2b3c4d5e',
-  database : 'temperatures'
-});
-connection.connect();
 
 const io = require('socket.io')(server);
 
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '1a2b3c4d5e',
+    database: 'temperatures',
+});
+
 app.use(express.static('../frontend/assets'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
 
 app.get('*', (req, res) => {
-  res.sendFile('C:/Users/Tom/Desktop/pss_rocnikovka/frontend/index.html');
+    res.sendFile('C:/Users/Tom/Desktop/pss_rocnikovka/frontend/index.html');
 });
 io.on('connection', (socket) => {
-  connection.query('SELECT * FROM log ORDER BY ID DESC,ID desc LIMIT 12',(error, results, fields) => {
-    if(error) {
-      socket.emit('connection', "false");
-      return;
-    }
-    const returnData = [];
-    for(let i = 0; i < results.length; i++) {
-      const splitted = results[i].Datum.toString().split(' ');
-      returnData.push({time: `${splitted[1]} ${splitted[2]} ${splitted[3]}, ${splitted[4]}`, value: results[i].Hodnota});
-    }
-    socket.emit('update', JSON.stringify(returnData));
-  });
-  socket.on('requestChartdata', (data) => {
-    d = JSON.parse(data);
-    console.log(d);
-    let x = d.from.split('-');
-    let y = d.to.split('-');
-    connection.query(`SELECT COUNT(ID) as Count FROM log WHERE Datum >= '${x[0]}-${x[1]}-${x[2]} ${x[3]}:${x[4]}:00' AND Datum < '${y[0]}-${y[1]}-${y[2]} ${y[3]}:${y[4]}:00'`, (error, results, fields) => {
-      if(error) {
-        socket.emit('sendChartdata', 'false');
-        return;
-      }
-      let i = 0;
-      if(results[0].Count < 50) {
+    pool.getConnection((error, connection) => {
+        if (error) {
+            socket.emit('DB_error', 'Chyba při snaze se spojit s databází.');
 
-      }
-      else if(results[0].Count){
-        let j = results[0].Count;
-        for(; j > 50; i++){
-          j /= 2;
+            return;
         }
-      }
-      else{
-        let j = results[0].Count - 1;
-        for(; j > 50; i++){
-          j /= 2;
-        }
-      }
-      let query = `SELECT * FROM (SELECT @row := @row +1 AS rownum, log.* FROM (SELECT @row := 0) r, log) ranked WHERE rownum % ${Math.pow(2,i)} = 0 AND Datum >= '${x[0]}-${x[1]}-${x[2]} ${x[3]}:${x[4]}:00' AND Datum < '${y[0]}-${y[1]}-${y[2]} ${y[3]}:${y[4]}:00'`;
-      connection.query(query, (error1, results1, fields1) => {
-        if(error1) {
-          socket.emit('sendChartdata', 'false');
-          return;
-        }
-        console.log(error1);
-        const returnData = [];
-        for(let i = 0; i<results1.length; i++){
-          const splitted = results1[i].Datum.toString().split(' ');
-          returnData.push({label: `${splitted[1]} ${splitted[2]} ${splitted[3]}, ${splitted[4]}`, value: results1[i].Hodnota})
-        }
-        socket.emit('sendChartdata', JSON.stringify(returnData));
-      });
+        connection.query('SELECT * FROM log ORDER BY ID DESC,ID desc LIMIT 12', (error, results) => {
+            connection.release();
+            if (error) {
+                socket.emit('DB_error', 'Chyba při komunikaci s databází.');
+
+                return;
+            }
+            const returnData = [];
+
+            for (let i = 0; i < results.length; i += 1) {
+                const splitted = results[i].Datum.toString().split(' ');
+
+                returnData.push({
+                    time: `${splitted[1]} ${splitted[2]} ${splitted[3]}, ${splitted[4]}`,
+                    value: results[i].Hodnota,
+                });
+            }
+            socket.emit('update', JSON.stringify(returnData));
+        });
     });
-  });
+    socket.on('requestChartdata', (data) => {
+        const d = JSON.parse(data);
+
+        const x = d.from.split('-');
+        const y = d.to.split('-');
+
+        try {
+            checkInput(...x);
+            checkInput(...y);
+
+            pool.getConnection((error, connection) => {
+                if (error) {
+                    socket.emit('DB_error', 'Chyba při snaze se spojit s databází.');
+
+                    return;
+                }
+                connection.query(`SELECT COUNT(ID) as Count FROM log
+                WHERE Datum >= '${x[0]}-${x[1]}-${x[2]} ${x[3]}:${x[4]}:00'
+                AND Datum < '${y[0]}-${y[1]}-${y[2]} ${y[3]}:${y[4]}:00'`, (error, results) => {
+                    if (error) {
+                        socket.emit('DB_error', 'Chyba při komunikaci s databází.');
+                        socket.emit('sendChartdata', 'false');
+
+                        return;
+                    }
+                    let i = 0;
+
+                    if (results[0].Count < 50) {
+                        console.log('stfulintr');
+                    }
+                    else if (results[0].Count) {
+                        let j = results[0].Count;
+
+                        for (; j > 50; i += 1) {
+                            j /= 2;
+                        }
+                    }
+                    else {
+                        let j = results[0].Count - 1;
+
+                        for (; j > 50; i += 1) {
+                            j /= 2;
+                        }
+                    }
+                    const query = `SELECT * FROM
+                    (SELECT @row := @row +1 AS rownum, log.* FROM
+                        (SELECT @row := 0) r, log) ranked
+                        WHERE rownum % ${2 ** i} = 0
+                        AND Datum >= '${x[0]}-${x[1]}-${x[2]} ${x[3]}:${x[4]}:00'
+                        AND Datum < '${y[0]}-${y[1]}-${y[2]} ${y[3]}:${y[4]}:00')`;
+
+                    connection.query(query, (error1, results1) => {
+                        connection.release();
+                        if (error1) {
+                            socket.emit('sendChartdata', 'false');
+
+                            return;
+                        }
+                        const returnData = [];
+
+                        for (let i = 0; i < results1.length; i += 1) {
+                            const splitted = results1[i].Datum.toString().split(' ');
+
+                            returnData.push({
+                                label: `${splitted[1]} ${splitted[2]} ${splitted[3]}, ${splitted[4]}`,
+                                value: results1[i].Hodnota,
+                            });
+                        }
+                        socket.emit('sendChartdata', JSON.stringify(returnData));
+                    });
+                });
+            });
+        }
+        catch (err) {
+            socket.emit('input_error', err.message);
+        }
+    });
 });
 
 const updateClients = () => {
-  connection.query('SELECT * FROM log ORDER BY ID DESC,ID desc LIMIT 1',(error, results, fields) => {
-    if(error) {
-      io.socket.emit('update', 'false');
-      return;
+    pool.getConnection((error, connection) => {
+        if (error) {
+            return;
+        }
+        connection.query('SELECT * FROM log ORDER BY ID DESC,ID desc LIMIT 1', (error, results) => {
+            connection.release();
+            if (error) {
+                return;
+            }
+            const returnData = [];
+
+            for (let i = 0; i < results.length; i += 1) {
+                const splitted = results[i].Datum.toString().split(' ');
+
+                returnData.push({
+                    time: `${splitted[1]} ${splitted[2]} ${splitted[3]}, ${splitted[4]}`,
+                    value: results[i].Hodnota,
+                });
+            }
+            io.sockets.emit('update', JSON.stringify(returnData));
+        });
+    });
+};
+const checkInput = (y, m, d, h, mn) => {
+    if ([y, m, d, h, mn].includes(undefined) || [y, m, d, h, mn].includes('')) {
+        throw new Error('Některá z políček nejsou vyplněná!');
     }
-    let returnData = [];
-    for(let i = 0; i < results.length; i++) {
-      const splitted = results[i].Datum.toString().split(' ');
-      returnData.push({time: `${splitted[1]} ${splitted[2]} ${splitted[3]}, ${splitted[4]}`, value: results[i].Hodnota});
+    else if (m < 1 || m > 12) {
+        throw new Error(`Tento měsíc neexistuje! (${m})`);
     }
-    io.sockets.emit('update', JSON.stringify(returnData));
-  });
-}
+    const maxdays = { _1: '31', _2: '28', _3: '31', _4: '30', _5: '31', _6: '30', _7: '31', _8: '31', _9: '30', _10: '31', _11: '30', _12: '31' };
+
+    if (d < '1' || d > maxdays[`_${m}`]) {
+        throw new Error(`Tento den neexistuje! ${d}`);
+    }
+    else if (h < '0' || h > '23') {
+        throw new Error(`Tato hodina neexistuje! ${h}`);
+    }
+    else if (mn < '0' || mn > '59') {
+        throw new Error(`Tato minuta neexistuje! ${mn}`);
+    }
+    else if (new Date(y, m, d, h, mn) > new Date()) {
+        throw new Error('Záznamy z budoucnosti nemohou existovat!');
+    }
+};
 
 server.listen(80);
--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,164 +1,126 @@
+'use strict';
+
 const socket = io.connect('http://localhost:80');
+const tableData = [];
 
 const fillTable = (data, update) => {
-  if(!update) {
-    for(let i = 0; i < data.length; i++) {
-      $('#entriesTable').append(`<tr><th>${data[i].time}</th><th>${data[i].value}</th></tr>`);
+    if (!update) {
+        for (let i = 0; i < data.length; i++) {
+            if (!(tableData.map((item) => item.time).includes(data[i].time))) {
+                $('#entriesTable').append(`<tr><th>${data[i].time}</th><th>${data[i].value}</th></tr>`);
+                tableData.push(data[i]);
+            }
+        }
+    } else {
+        $('#entriesTable').append(`<tr><th>${data.time}</th><th>${data.value}</th></tr>`);
     }
-  }
-  else {
-    $('#entriesTable').append(`<tr><th>${data.time}</th><th>${data.value}</th></tr>`);
-  }
 }
 
-$('#showForm').submit((event)=>{
-  //PODMÍNKY IMPLEMENTOVAT I NA SERVER
-  event.preventDefault();
-  let fromD = [$('input[name=fY]').val(),$('input[name=fM]').val(),$('input[name=fD]').val(),$('input[name=fH]').val(),$('input[name=fMM]').val()];
-  let toD = [$('input[name=tY]').val(),$('input[name=tM]').val(),$('input[name=tD]').val(),$('input[name=tH]').val(),$('input[name=tMM]').val()];
+$('#showForm').submit((event) => {
+    //PODMÍNKY IMPLEMENTOVAT I NA SERVER
+    event.preventDefault();
+    const fromD = [$('input[name=fY]').val(), $('input[name=fM]').val(), $('input[name=fD]').val(), $('input[name=fH]').val(), $('input[name=fMM]').val()];
+    const toD = [$('input[name=tY]').val(), $('input[name=tM]').val(), $('input[name=tD]').val(), $('input[name=tH]').val(), $('input[name=tMM]').val()];
 
-  if(checkDatetimeFormat(fromD))
-    return;
-  if(checkDatetimeFormat(toD))
-    return;
-  console.log(":DDD")
-  socket.emit('requestChartdata', JSON.stringify({
-    from: fromD.join('-'),
-    to: toD.join('-')
-  }));
+    try {
+        checkInput(...fromD);
+        checkInput(...toD);
+        socket.emit('requestChartdata', JSON.stringify({
+            from: fromD.join('-'),
+            to: toD.join('-'),
+        }));
+    }
+    catch (err) {
+        alert(err.message);
+    }
 });
 
 
 
-const checkDatetimeFormat = (datetime) => {
-  if(datetime.includes(undefined) || datetime.includes('')){
-    throwError('Některé z políček není vyplněné!');
-    return true;
-  }
-  else if(datetime[0] < '2000'){
-    throwError('Záznamy z tohoto roku nemůžou existovat!');
-    return true;
-  }
-  else if(datetime[1] < 1 || datetime[1] > 12){
-    throwError("Tento měsíc neexistuje!");
-    return true;
-  }
-  else{
-    let maxdays;
-    switch(datetime[1]){
-      case 1:
-        maxdays = '31';
-        break;
-      case 2:
-        maxdays = '28';
-        break;
-      case 3:
-        maxdays = '31';
-        break;
-      case 4:
-        maxdays = '30';
-        break;
-      case 5:
-        maxdays = '31';
-       break;
-      case 6:
-        maxdays = '30';
-        break;
-      case 7:
-        maxdays = '31';
-        break;
-      case 8:
-        maxdays = '31';
-        break;
-      case 9:
-        maxdays = '30';
-        break;
-      case 10:
-        maxdays = '31';
-        break;
-      case 11:
-        maxdays = '30';
-        break;
-      case 12:
-        maxdays = '31';
-        break;
+const checkInput = (y, m, d, h, mn) => {
+    if ([y, m, d, h, mn].includes(undefined) || [y, m, d, h, mn].includes('')) {
+        throw new Error('Některá z políček nejsou vyplněná!');
     }
-    if(datetime[2] < '1' || datetime[2] > maxdays) {
-      throwError('Tento den neexistuje!');
-      return true;
+    else if (m < 1 || m > 12) {
+        throw new Error(`Tento měsíc neexistuje! (${m})`);
     }
-    else if(datetime[3] < '0' || datetime[3] > '23') {
-      throwError('Tato hodina neexistuje!');
-      return true;
+    const maxdays = { _1: '31', _2: '28', _3: '31', _4: '30', _5: '31', _6: '30', _7: '31', _8: '31', _9: '30', _10: '31', _11: '30', _12: '31' };
+
+    if (d < '1' || d > maxdays[`_${m}`]) {
+        throw new Error(`Tento den neexistuje! ${d}`);
     }
-    else if(datetime[4] < '0' || datetime[4] > '59') {
-      throwError('Tato Minuta neexistuje!');
-      return true;
+    else if (h < '0' || h > '23') {
+        throw new Error(`Tato hodina neexistuje! ${h}`);
     }
-    else {
-      return false;
+    else if (mn < '0' || mn > '59') {
+        throw new Error(`Tato minuta neexistuje! ${mn}`);
     }
-  }
-}
+    else if (new Date(y, m, d, h, mn) > new Date()) {
+        throw new Error('Záznamy z budoucnosti nemohou existovat!');
+    }
+};
+
 
 const throwError = (message) => {
-  alert(message);
+    alert(message);
 }
+socket.on('input_error', msg => alert(msg));
+
+socket.on('DB_error', msg => alert(msg));
 
 socket.on('update', data => fillTable(JSON.parse(data), data.length < 2));
 
 socket.on('sendChartdata', (data) => {
-  if(!JSON.parse(data)) {
-    console.log('kkt')
-    return;
-  }
-  $('#chartgen').insertFusionCharts({
-    type: 'line',
-    width: '100%',
-    height: '100%',
-    dataFormat: 'json',
-    dataSource: {
-       'chart': {
-           'caption': `Teplota v intervalu od ${JSON.parse(data)[0].label} do ${JSON.parse(data)[JSON.parse(data).length-1].label}`,
-           'xAxisName': 'Čas',
-           'yAxisName': 'Teplota v °C',
-           'baseFontSize': '15',
+    if (!JSON.parse(data)) {
+        return;
+    }
+    $('#chartgen').insertFusionCharts({
+        type: 'line',
+        width: '100%',
+        height: '100%',
+        dataFormat: 'json',
+        dataSource: {
+            'chart': {
+                'caption': `Teplota v intervalu od ${JSON.parse(data)[0].label} do ${JSON.parse(data)[JSON.parse(data).length-1].label}`,
+                'xAxisName': 'Čas',
+                'yAxisName': 'Teplota v °C',
+                'baseFontSize': '15',
 
-           //Cosmetics
-           'animation': '1',
-           'animationDuration': '1.25',
-           'lineThickness': '2',
-           'paletteColors': '#62a861',
-           'baseFontColor': '#000',
-           'baseFont':  'Source Sans Pro, sans-serif',
-           'captionFontSize': '30',
-           'showBorder': '0',
-           'bgColor': '#ffffff',
-           'showShadow': '0',
-           'canvasBgColor': '#ffffff',
-           'canvasBorderAlpha': '0',
+                //Cosmetics
+                'animation': '1',
+                'animationDuration': '1.25',
+                'lineThickness': '2',
+                'paletteColors': '#62a861',
+                'baseFontColor': '#000',
+                'baseFont': 'Source Sans Pro, sans-serif',
+                'captionFontSize': '30',
+                'showBorder': '0',
+                'bgColor': '#ffffff',
+                'showShadow': '0',
+                'canvasBgColor': '#ffffff',
+                'canvasBorderAlpha': '0',
 
-           'divlineAlpha': '100',
-           'divlineColor': '#000',
-           'divlineThickness': '1',
-           'divLineIsDashed': '1',
-           'divLineDashLen': '5',
+                'divlineAlpha': '100',
+                'divlineColor': '#000',
+                'divlineThickness': '1',
+                'divLineIsDashed': '1',
+                'divLineDashLen': '5',
 
-           'anchorRadius': '5',
-           'anchorBorderThickness': '2',
+                'anchorRadius': '5',
+                'anchorBorderThickness': '2',
 
-           'showXAxisLine': '1',
-           'showYAxisLine': '1',
-           'xAxisLineThickness': '3',
-           'yAxisLineThickness': '3',
-           'xAxisLineColor': '#999999',
-           'yAxisLineColor': '#999999',
-           'yAxisNameFontSize': '20',
-           'xAxisNameFontSize': '20',
-           'showAlternateHGridColor': '0',
+                'showXAxisLine': '1',
+                'showYAxisLine': '1',
+                'xAxisLineThickness': '3',
+                'yAxisLineThickness': '3',
+                'xAxisLineColor': '#999999',
+                'yAxisLineColor': '#999999',
+                'yAxisNameFontSize': '20',
+                'xAxisNameFontSize': '20',
+                'showAlternateHGridColor': '0',
 
-       },
-       'data': JSON.parse(data)
-   }
-  });
+            },
+            'data': JSON.parse(data)
+        }
+    });
 });
